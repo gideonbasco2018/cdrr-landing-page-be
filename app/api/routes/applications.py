652f706b903349d4b1dc8fe9599fcc2d644525c9
Gideon Arianne E. Basco <gideonbasco2018@gmail.com>
2026-08-12 18:45:20 +0800

    Update backend

--- app/api/routes/applications.py
+++ app/api/routes/applications.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.schemas.applications import ApplicationCreate, ApplicationResponse
 from app.crud import applications as crud_application
+from app.core.security_external import verify_bearer_token
 
 router = APIRouter(prefix="/applications", tags=["Applications"])
 
 
-@router.post("/", response_model=ApplicationResponse)
+@router.post(
+    "/",
+    response_model=ApplicationResponse,
+    dependencies=[Depends(verify_bearer_token)],
+)
 def create_application(payload: ApplicationCreate, db: Session = Depends(get_db)):
     return crud_application.create_application(db, payload)
